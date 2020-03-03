7fdf0f42c50c4bc1ce4131c0d523ae1564f00db1
idevlab <idevlab@outlook.com>
2020-03-03 10:08:40 +0800

    add background img and some button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to H5 Game App" />
+    <div class="background"></div>
+    <div class="buttom">
+      <el-button type="primary" icon="el-icon-edit"></el-button>
+      <el-button type="primary" icon="el-icon-edit"></el-button>
+      <el-button type="primary" icon="el-icon-edit"></el-button>
+      <el-button
+        type="primary"
+        icon="el-icon-setting"
+        @click="settingClicked"
+      ></el-button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  methods: {
+    settingClicked() {
+      this.$router.push("/about");
+    }
   }
 };
 </script>
+<style scoped>
+.background {
+  background-image: url(/img/city1.jpg);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-size: cover;
+  z-index: -1;
+}
+.buttom {
+  bottom: 0.5rem;
+  margin: 0.5rem;
+  position: fixed;
+}
+</style>
