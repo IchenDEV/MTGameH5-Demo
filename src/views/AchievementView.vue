6cebf504f9afc3dc2b5e9f23f62a26daa08f78cd
st <1120161630@qq.com>
2020-04-04 17:51:01 +0800

    添加了成就和好友界面

--- src/views/AchievementView.vue
+++ src/views/AchievementView.vue
@@ -1,6 +1,29 @@
 <template>
   <bottom-panel>
     <h2>成就</h2>
+    <el-tabs class="tabs" tab-position="left">
+      <el-tab-pane label="成就1">
+        <div>
+          <p style="color:#9475FF;line-height:5px">成就1是这个游戏的第一个成就</p>
+          <h4 style="line-height:5px;align:center;">当前进度</h4>
+          <progress value="80" max="100"></progress>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="成就2">
+          <div>
+          <p style="color:#9475FF;line-height:5px">成就2是这个游戏的第二个成就</p>
+          <h4 style="line-height:5px;align:center;">当前进度</h4>
+          <progress value="60" max="100"></progress>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="成就3">
+          <div>
+          <p style="color:#9475FF;line-height:5px">成就3是这个游戏的第三个成就</p>
+          <h4 style="line-height:5px;align:center;">当前进度</h4>
+          <progress value="30" max="100"></progress>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </bottom-panel>
 </template>
 
